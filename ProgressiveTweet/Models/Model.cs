cf29ca699ce97234c47af9e4d6f9294f71e52efc
paralleltree <paralleltree@outlook.com>
2015-02-15 10:10:12 +0900

    Modelクラスをシングルトン化
    アクセストークンを保持するプロパティを追加。

--- ProgressiveTweet/Models/Model.cs
+++ ProgressiveTweet/Models/Model.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Text;
 
 using Livet;
+using CoreTweet;
 
 namespace ProgressiveTweet.Models
 {
     public class Model : NotificationObject
     {
+        private Model()
+        {
+        }
+
+        #region Singleton
+        private static Model _instance;
+        public static Model Instance
+        {
+            get { return _instance ?? (_instance = new Model()); }
+        }
+        #endregion
+
         /*
          * NotificationObjectはプロパティ変更通知の仕組みを実装したオブジェクトです。
          */
+
+        /// <summary>
+        /// Twitterへのアクセストークンを取得、設定します。
+        /// </summary>
+        public Tokens CurrentToken { get; set; }
     }
 }
